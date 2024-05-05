3e8d7c7a4f61e4ba5df464e5d36e8ca96051b526
Vaasu02 <chourasiavasu88@gmail.com>
2024-05-05 12:39:00 +0530

    updated

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 
 const express=require('express');
 const expresslayouts=require('express-ejs-layouts');
+const methodivveride=require('method-override');
 const connectDB=require('./server/config/db');
 const session=require('express-session');
 const passport=require('passport');
@@ -14,6 +15,7 @@ const port=process.env.PORT || 3000;
 
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
+app.use(methodivveride("_method"))
 
 
 
